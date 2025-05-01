643418ef92c59135c1a3b25a4e38fe7643451ac5
Jonathan Reymond <johndoeteddy1@gmail.com>
2025-05-01 05:52:51 -0400

    added server error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,11 +1,18 @@
 import requests
 import json
 
+ 
 
 def emotion_detector(text_to_analyse):
     '''
     Retrieve the emotion score given by the Watson Emotion predict tool that appears in the given text
     '''
+    blank_dict = {'anger': None, 'disgust': None, 'fear': None, 'joy': None,
+                 'sadness': None, "dominant_emotion": None}
+
+    if text_to_analyze.strip():
+        return blank_dict
+        
 
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
@@ -14,6 +21,9 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json=myobj, headers=headers)
 
     formatted_response = json.loads(response.text)
+
+    if response.status_code == 400:
+        return blank_dict
     emotions = formatted_response['emotionPredictions'][0]['emotion']
     emotions['dominant_emotion'] = max(emotions, key=emotions.get)
 
--- server.py
+++ server.py
@@ -12,6 +12,9 @@ def sent_analyzer():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
+    if not response["dominant_emotion"]:
+        return "<b>Invalid text! Please try again!.</b>"
+
     output = "For the given statement, the system response is "
     dominant_emotion = response.pop("dominant_emotion")
 
